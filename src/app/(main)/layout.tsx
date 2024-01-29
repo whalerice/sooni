a7d4f0d3e4926eb2b3d8776ccb144e5a45b635ec
taerim.woo <taerim.woo@quantum-ai.ai>
2024-01-29 17:44:37 +0900

    fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,9 +2,8 @@ import MainLayout from '@/ui/main-layout';
 import { cookies } from 'next/headers';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  let mode: any = cookies().has('theme-mode')
-    ? cookies().get('theme-mode')?.value
-    : 'light';
+  const mode = cookies().get('theme-mode')?.value;
+
   return (
     <>
       <MainLayout themeMode={mode}>{children}</MainLayout>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,15 @@
 'use client';
-
-import { Input, Typography } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Input, Select, Typography } from 'antd';
 const { Title, Text, Paragraph } = Typography;
 
 export default function Page() {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
   return (
     <>
+      <Avatar size={64} icon={<UserOutlined />} />
       <Title>대시보드</Title>
       <Input placeholder="test" />
       <p>test</p>
@@ -15,6 +19,18 @@ export default function Page() {
         iste, rem aut dicta a eveniet fugit, quaerat itaque beatae quidem, quo
         numquam consequuntur tenetur vel deserunt ipsam nulla ut!
       </Paragraph>
+      <Button type="primary">Primary Button</Button>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+        options={[
+          { value: 'jack', label: 'Jack' },
+          { value: 'lucy', label: 'Lucy' },
+          { value: 'Yiminghe', label: 'yiminghe' },
+          { value: 'disabled', label: 'Disabled', disabled: true },
+        ]}
+      />
     </>
   );
 }
